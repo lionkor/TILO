9412022098aef5509928adf57c956d51052cd967
Lion Kortlepel <development@kortlepel.com>
2020-04-29 09:50:28 +0200

    Add compiled praktikum_2 file

--- src/praktikum-2/praktikum_2.pl
+++ src/praktikum-2/praktikum_2.pl
@@ -28,3 +28,24 @@ rev(nil, nil).
 rev(list(X, nil), list(X, nil)).
 rev(list(X, Xs), Ys) :- rev(Xs, Rests), attach(Rests, X, Ys), linListe(Xs), linListe(Ys).
 
+% node(eintrag, lb, rb): Ein node mit root=eintrag und lb und rb als linker bzw rechter Baum
+node(eintrag, lb, rb).
+
+% binbaum(X): X ist ein binaerer Baum.
+binbaum(empty).
+binbaum(node(_, Lb, Rb)) :- binbaum(Lb), binbaum(Rb).
+
+% construct(Root,Lb,Rb,Xneub): Xneub ist der Baum mit Wurzelbeschriftung Root, linkem Teilbaum Lb und rechtem Teilbaum Rb.
+construct(Root, Lb, Rb, node(Root, Lb, Rb)) :- binbaum(Lb), binbaum(Rb).
+
+% nat(X): X ist eine natuerliche Zahl.
+nat(o).
+nat(s(X)) :- nat(X).
+
+% add(X, Y, Z): Z = X + Y
+add(o, X, X) :- nat(X).
+add(s(N1), N2, s(N3)) :- nat(N1), nat(N2), nat(N3), add(N1, N2, N3).
+
+% knotenanz(Xb,N): N (nat. Zahl in symbolischer Darstellung) ist die Anzahl der Knoten des Baumes Xb.
+knotenanz(empty, o).
+knotenanz(node(_, Lb, Rb), s(N3)) :- knotenanz(Lb, N1), knotenanz(Rb, N2), add(N1, N2, N3), nat(N1), nat(N2), nat(N3), binbaum(Lb), binbaum(Rb).
